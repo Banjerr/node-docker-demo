68f005470efd1936882bf6415c609668d9d62849
Ben Redden <benjamminredden@gmail.com>
2017-05-13 09:09:51 -0400

    updated Dockerfile/compose file, added actual angular app + node api w/ express routing

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,18 @@
-# The Base Image
-## This is always the first line of a Dockerfile
-### Here we're starting off with the latest LTS (long term support) version boron of node available from the Docker Hub
-## the FROM format can use different formats, like <image>:<tag> OR <image>
-# when using <image>, Docker will pull the latest version from the Docker hub if one isn't found locally
-FROM node:boron
+# Gonna start with a minimal Linux distro called Alpine with node installed
+FROM mhart/alpine-node:latest
 
 # Create app directory
 ## Here we are creating the directory "/usr/src/app"
 ## And then sets the WORKDIR to that directory
 # Which sets the desired directory for RUN, CMD, ENTRYPOINT, COPY and ADD instructions that follow it in the Dockerfile
-RUN mkdir -p /usr/src/app
+RUN mkdir -p /usr/src/app && mkdir -p /usr/src/data/db
 WORKDIR /usr/src/app
 
 # Install app dependencies
-## Here we copy the package.json containing dependencies 
+## Here we copy the package.json containing dependencies
 # And then we use the NPM binary to install all the things
 COPY package.json /usr/src/app/
-RUN npm install
+RUN npm install && npm cache clean
 
 # Bundle app source
 ## Here we're just copying over the code for the app
@@ -24,7 +20,7 @@ RUN npm install
 COPY . /usr/src/app
 
 # Expose the right port
-## The EXPOSE instruction tells Docker which port to map for the app 
+## The EXPOSE instruction tells Docker which port to map for the app
 EXPOSE 8080
 
 # Finally, the instruction which tells Docker it's time to start the show
--- server.js
+++ server.js
@@ -1,14 +1,89 @@
 'use strict';
 
+// set up
 const express = require('express');
 const PORT = 8080;
 const app = express();
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
-// App
-app.get('/', function (req, res) {
-  res.send('Hello world\n');
+// config
+mongoose.connect('mongodb://mongodb:27017');
+app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+app.use(morgan('dev')); // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
+
+// define model =================
+var Todo = mongoose.model('Todo', {
+    text : String
 });
 
+// routes ======================================================================
+  // api ---------------------------------------------------------------------
+  // get all todos
+  app.get('/api/todos', function(req, res) {
+
+      // use mongoose to get all todos in the database
+      Todo.find(function(err, todos) {
+
+          // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+          if (err)
+              res.send(err)
+
+          res.json(todos); // return all todos in JSON format
+      });
+  });
+
+  // create todo and send back all todos after creation
+  app.post('/api/todos', function(req, res) {
+
+      // create a todo, information comes from AJAX request from Angular
+      Todo.create({
+          text : req.body.text,
+          done : false
+      }, function(err, todo) {
+          if (err)
+              res.send(err);
+
+          // get and return all the todos after you create another
+          Todo.find(function(err, todos) {
+              if (err)
+                  res.send(err)
+              res.json(todos);
+          });
+      });
+
+  });
+
+  // delete a todo
+  app.delete('/api/todos/:todo_id', function(req, res) {
+      Todo.remove({
+          _id : req.params.todo_id
+      }, function(err, todo) {
+          if (err)
+              res.send(err);
+
+          // get and return all the todos after you create another
+          Todo.find(function(err, todos) {
+              if (err)
+                  res.send(err)
+              res.json(todos);
+          });
+      });
+  });
+
+
+// application -------------------------------------------------------------
+app.get('*', function(req, res) {
+    res.sendfile('./public/index.ng.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
+
+// App
 app.listen(PORT);
 
 console.log('Running on http://localhost:' + PORT);
